36c295a38caa5d1de9ee63792d794d3d57f9bf9c
carlosoliveira397 <36646274+carlosoliveira397@users.noreply.github.com>
2024-05-18 23:50:57 -0300

    :construction: build Connection webListener
    Adicionando vinculo a Classe com o servidor

--- LARosaWebApp/src/java/web/AppListener.java
+++ LARosaWebApp/src/java/web/AppListener.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package web;
 
-/**
- *
- * @author Fatec
- */
-public class AppListener {
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import java.util.Date;
+
+import java.sql.*;
+import model.Imovel;
+
+@WebListener
+public class AppListener implements ServletContextListener{
+    public static final String CLASS_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:laroraapp.db";
+    public static String initializeLog = "";
+    public static Exception exception = null;
     
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try{
+            Connection c = AppListener.getConnection();
+            Statement s = c.createStatement();
+            initializeLog += new Date() + ": Initializing database creation; ";
+            initializeLog += "Creating Users table if not exist...";
+            s.execute(Imovel.getCreateStatement());
+            initializeLog += "done. ";
+            s.close();
+            c.close();
+        }catch(Exception ex){
+            initializeLog += "Erro: " + ex.getMessage();
+            exception = ex;
+        }
+    }
+    
+    public static Connection getConnection() throws Exception{
+        Class.forName(CLASS_NAME);
+        return DriverManager.getConnection(URL);
+    }
 }
